d2cceeec6257ac300ee0d5dae7de7ae36e47444d
Test User <test@example.com>
2026-07-02 02:34:53 +0530

    Add local Angular development config to point api_url to localhost:5200/api

--- Angular/Taskverse/src/environments/environment.dev.ts
+++ Angular/Taskverse/src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    configFileName: 'assets/config.json',
+    configFileName: 'assets/config.dev.json',
     currentYear: new Date().getFullYear(),
     proxy: false,
     proxyEnvURL: null
